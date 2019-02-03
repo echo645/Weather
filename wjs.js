88dcbbcda995f7efdb8c2f05206935641cb2ba73
echo645 <2420057424@qq.com>
2019-02-03 17:48:46 +0800

    更改了切换方式，添加了点击查看一周天气功能

--- wjs.js
+++ wjs.js
@@ -1,3 +1,6 @@
+var chengduArray=new Array("成都","4","阴");
+var beijingArray=new Array("北京","-2","雪");
+var hangzhouArray=new Array("杭州","5","晴");
 function startTime() {
 				var today = new Date();
 				var h = today.getHours();
@@ -18,27 +21,46 @@ function startTime() {
         document.getElementById('date-txt').innerHTML =
 				year + "/" + month + "/" + daym+" " +dayarray[day];
 				var t = setTimeout(startTime, 500);
-				}
 				function checkTime(i) {
 				if (i < 10) {i = "0" + i}; 
 				return i;
+        }
 }
 function OpenDiv(){
-document.getElementById("city-select-id").style.display="block";
-//document.getElementById("changepalce-id").style.display="none";
+  document.getElementById("city-select-id").style.display="block";
+}
+function changecity(cityArray){
+  document.getElementById("palce-id").innerHTML=cityArray[0];
+  document.getElementById("temperature-id").innerHTML=cityArray[1];
+  document.getElementById("weather-info-id").innerHTML=cityArray[2];
 }
 function CloseDivSubmit(){
-document.getElementById("city-select-id").style.display="none";
-//window.location.href='beijing.html';
-var sel=document.getElementById("js-change-city");
-if(sel.selectedIndex.value=="成都"){//obj.selectedIndex
-  window.location.href='index.html';
-}else if(sel.selectedIndex.value=="北京"){
-  window.location.href='beijing.html';
-}else if(sel.selectedIndex.value=="hangzhou"){
-  window.location.href='hangzhou.html';
-}
+  document.getElementById("city-select-id").style.display="none";
+  var sel=document.getElementById("js-change-city");
+  if(sel.value=="成都"){
+    changecity(chengduArray);
+  }else if(sel.value=="北京"){
+    changecity(beijingArray);
+  }else if(sel.value=="杭州"){
+    changecity(hangzhouArray);
+  }
 }
 function CloseDivCancel(){
-document.getElementById("city-select-id").style.display="none";
+  document.getElementById("city-select-id").style.display="none";
+}
+function showMore(){
+  document.getElementById("weather-week-section-4-li").style.display="inline-block";
+  document.getElementById("weather-week-section-5-li").style.display="inline-block";
+  document.getElementById("weather-week-section-6-li").style.display="inline-block";
+  document.getElementById("weather-week-section-7-li").style.display="inline-block";
+  document.getElementById("weather-week-section-8-li").style.display="none";
+  document.getElementById("weather-week-section-9-li").style.display="inline-block";
+}
+function pickUp(){
+  document.getElementById("weather-week-section-4-li").style.display="none";
+  document.getElementById("weather-week-section-5-li").style.display="none";
+  document.getElementById("weather-week-section-6-li").style.display="none";
+  document.getElementById("weather-week-section-7-li").style.display="none";
+  document.getElementById("weather-week-section-8-li").style.display="inline-block";
+  document.getElementById("weather-week-section-9-li").style.display="none";
 }
